c0874d4ad78d73ad3e6c70064b97a70573855bb9
pedrokorb <pedro.korb@gmail.com>
2020-07-01 19:09:37 -0300

    fix images directory test

--- src/components/Customized/Banners/BannerCarousel.js
+++ src/components/Customized/Banners/BannerCarousel.js
@@ -62,7 +62,7 @@ export default function BannerCarousel(props) {
       buttonLink: "https://www.google.com.br",
       buttonColor: '#FFF', 
       buttonTextColor: '#2D2A7E',
-      image: '../../../../content/assets/casa.jpg' 
+      image: '../../../../public/images/casa.jpg' 
     },
     // { 
     //   title: "Cavalo",
